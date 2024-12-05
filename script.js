de772977b236b649f48687a0b70f6c7583209c5f
Ryan Hirte <rhirte@ewu.edu>
2024-12-05 11:45:30 -0800

    js for date done

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Check JS is Connected
-console.log('JS Connected');
+console.log("JS Connected");
+
+// Get the Footer
+const footer = document.querySelector(".copyright");
+
+// Get the Current Year
+const date = new Date();
+const year = date.getFullYear();
+console.log(year);
+
+// Set the footer to have the current year and copyright
+footer.innerHTML = `&copy; ${year} RyanHirte`;
